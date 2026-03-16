f0397c69c49c0544c17c8caa3f2d8b8f9b00516a
Sharath Kumar <kademarsharath@gmail.com>
2026-03-16 12:29:14 -0700

    Moved projects to pipeline folder

--- pipelines/demo_project/logs/query_log.sql
+++
@@ -1,9 +0,0 @@
--- created_at: 2026-03-13T22:47:36.501212+00:00
--- finished_at: 2026-03-13T22:47:36.783154+00:00
--- elapsed: 281ms
--- outcome: success
--- dialect: databricks
--- node_id: not available
--- query_id: 01f11f2e-a1a4-1ca3-b654-5fd83b6ace67
--- desc: not provided
-select 1 as id;
---
+++ pipelines/ingest_project1/models/marts/sales_customers.sql
@@ -0,0 +1,51 @@
+with
+
+customers as (
+
+    select * from {{ ref('stg_sales_customers') }}
+
+),
+
+orders as (
+
+    select * from {{ ref('stg_sales_transactions') }}
+
+),
+
+customer_orders_summary as (
+
+    select
+        orders.customer_id,
+
+        count(distinct orders.transaction_id) as count_lifetime_orders,
+        count(distinct orders.transaction_id) > 1 as is_repeat_buyer,
+        sum(orders.unit_price*orders.quantity ) as unit_prices,
+        sum(orders.quantity) as quantity
+
+    from orders
+
+    group by 1
+
+),
+
+joined as (
+
+    select
+        customers.*,
+
+        customer_orders_summary.count_lifetime_orders,
+        customer_orders_summary.unit_prices,
+        customer_orders_summary.quantity,
+        case
+            when customer_orders_summary.is_repeat_buyer then 'returning'
+            else 'new'
+        end as customer_type
+
+    from customers
+
+    left join customer_orders_summary
+        on customers.customer_id = customer_orders_summary.customer_id
+
+)
+
+select * from joined
